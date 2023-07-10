a58e3fb54711563e39c0c83f6f0fbf81cd292dc4
Husam Dayya <husamdayya@icloud.com>
2023-07-10 14:19:19 +0300

    Created an array of books an used it to render the names in table

--- MVVMtest/Model/Book.swift
+++ MVVMtest/Model/Book.swift
@@ -11,3 +11,9 @@ struct Book {
     var title, author, publisher, genre : String?
     var publishingDate, price : Int?
 }
+
+let books = [
+    Book(title: "The Stranger", author: "Albert Camus", publisher: "Penguin", genre: "Philosophy", publishingDate: 1942, price: 10),
+    Book(title: "1984", author: "George Orwell", publisher: "publisherX", genre: "Science Fiction", publishingDate: 1949, price: 8),
+    Book(title: "The Hunger Games", author: "Suzanne Collins", publisher: "publisherY", genre: "Science Fiction", publishingDate: 2008, price: 12)
+]
--- MVVMtest/ViewController.swift
+++ MVVMtest/ViewController.swift
@@ -11,34 +11,23 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
-    let names = ["The Stranger","1984","The Hunger Games"]
-    
-    private let viewModel: ViewModel = ViewModel()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
-        
-
     }
-
-
 }
 
-extension ViewController: UITableViewDelegate{
+extension ViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //print("test")
-        //i think this might need to go in viewmodel
         let vc = storyboard?.instantiateViewController(identifier: "expanded") as! ExpandedViewController
         
         let cell = tableView.cellForRow(at: (indexPath))
         vc.titleText = cell?.textLabel?.text
         present(vc, animated: true, completion: nil)
     }
-    
 }
 
 extension ViewController: UITableViewDataSource{
@@ -46,17 +35,15 @@ extension ViewController: UITableViewDataSource{
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return books.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = books[indexPath.row].title
         
-        cell.textLabel?.text = names[indexPath.row]
         return cell
     }
-    
-    
 }
-

5dd63d8e29ef363696ad8a5a5c073ea29a19d42f
Husam Dayya <husamdayya@icloud.com>
2023-07-10 14:37:53 +0300

    Removed unused function

--- MVVMtest/ViewController.swift
+++ MVVMtest/ViewController.swift
@@ -31,10 +31,6 @@ extension ViewController: UITableViewDelegate {
 
 extension ViewController: UITableViewDataSource {
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return books.count
     }
